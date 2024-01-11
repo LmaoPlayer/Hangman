08d472c59b1ae58e218230f5dc4da75e555b58d4
BuildTools <unconfigured@null.spigotmc.org>
2024-01-11 18:46:59 +0100

    Basic requirments met.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Hangman = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Hangman)).BeginInit();
             this.SuspendLayout();
+            // 
+            // Hangman
+            // 
+            this.Hangman.Location = new System.Drawing.Point(196, 43);
+            this.Hangman.Name = "Hangman";
+            this.Hangman.Size = new System.Drawing.Size(404, 314);
+            this.Hangman.TabIndex = 0;
+            this.Hangman.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(102, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "<";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(619, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = ">";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(370, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Hangman);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Hangman)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Hangman;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,12 +12,45 @@ namespace Les2AO
 {
     public partial class Form1 : Form
     {
+        //maakt de afbeeldingen aan
+        string[] afbeeldingen = { "G0.jpg", "G1.jpg", "G2.jpg", "G3.jpg", "G4.jpg", "G5.jpg", "G6.jpg", "G7.jpg", "G8.jpg", "G9.jpg", "G10.jpg"};
+        int huidigeAfbeelding = 0;
+        
+
         public Form1()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+            string imageName = $"images/{afbeeldingen[huidigeAfbeelding]}";
+            Console.WriteLine("imageName: " + imageName);
+            Hangman.Image = Image.FromFile(imageName);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Console.WriteLine("lengte afbeeldingen: " + afbeeldingen.Length);
+            if (0 < huidigeAfbeelding && huidigeAfbeelding < afbeeldingen.Length)
+            {
+                huidigeAfbeelding--;
+                Hangman.Image = Image.FromFile("images\\" + afbeeldingen[huidigeAfbeelding]);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (0 <= huidigeAfbeelding && huidigeAfbeelding < (afbeeldingen.Length-1))
+            {
+                huidigeAfbeelding++;
+                Hangman.Image = Image.FromFile("images\\" + afbeeldingen[huidigeAfbeelding]);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
